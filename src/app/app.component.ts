dffdfb27ba34fdb708b7005aea7481efb8b916cd
pranavbhagwat92 <pranavbhagwat81@gmail.com>
2020-05-03 22:13:20 +0530

    zone status detection added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent {
   recoveredSummary:number;
   deceasedSummary:number;
 
+  confirmed:number;
   recovered:number;
   hospitalized:number;
   deceased : number;
@@ -26,6 +27,7 @@ export class AppComponent {
 
   searchStateTerm:string = '';
   searchDistrictTerm:string = '';
+  zoneTerm :string= '';
   searchCityTerm:string = '';
 
   totalData = [];
@@ -39,10 +41,10 @@ export class AppComponent {
   }
 
  ngOnInit(): void {
-  console.log("In constructor ");
+  //console.log("In constructor ");
   this.covid.getStartingData().subscribe((response:any)=>{
     this.startingData = response;
-    console.log(this.startingData);
+    //console.log(this.startingData);
 
     for(let i=0;i<this.startingData.length;i++){
       if(this.startingData[i].state == "Total"){
@@ -57,21 +59,53 @@ export class AppComponent {
  }
 
 
+
   showData(){
 
     this.viewFlag = true;
-    console.log(this.searchStateTerm);
+    //console.log(this.searchStateTerm);
     if(this.searchStateTerm){
 
-      this.covid.getCovidData().subscribe((response : any)=>{
+      this.covid.getCovidData().subscribe(responseList => {
+        let responseData1:any = responseList[0];
+        let responseData2:any = responseList[1];
+        let responseData3:any = responseList[2];
 
-        console.log(this.searchTerm);
-        console.log("response",response);
+        let temparr = [];
+        let temparrIndex = 0;
+        var response = {
+         
+        };
         
+
+        for (let index = 0; index < responseData1.raw_data.length; index++) {
+          temparr[temparrIndex]= responseData1.raw_data[index];
+          temparrIndex++;
+        }
+
+        for (let index = 0; index < responseData2.raw_data.length; index++) {
+          temparr[temparrIndex]= responseData2.raw_data[index];
+          temparrIndex++;
+        }
+
+        for (let index = 0; index < responseData3.raw_data.length; index++) {
+          temparr[temparrIndex]= responseData2.raw_data[index];
+          temparrIndex++;
+        }
+
+        //console.log(responseData3);
+        response["raw_data"] = temparr;
+
+        //console.log(temparr);
+        //console.log(response["raw_data"]);
+
         this.totalData = [];
         this.filteredData = [] 
   
-        this.totalData = response.raw_data;
+        this.totalData = response["raw_data"];
+
+        //console.log(response);
+        //console.log(this.totalData);
   
         for (let index = 0; index < response["raw_data"].length; index++) {
           
@@ -88,16 +122,49 @@ export class AppComponent {
          
         }
   
-        console.log(this.filteredData);
+        //console.log(this.filteredData);
         this.getRecoveryStatus();
         this.status = true;
 		document.getElementById("Patientstable").focus();
+
+
+    });
+
+    //   this.covid.getCovidData().subscribe((response : any)=>{
+
+    //     console.log(this.searchTerm);
+    //     console.log("response",response);
+        
+    //     this.totalData = [];
+    //     this.filteredData = [] 
   
-      });
+    //     this.totalData = response.raw_data;
+  
+    //     for (let index = 0; index < response["raw_data"].length; index++) {
+          
+    //       let temp =  this.totalData[index];
+    //       let tempStatestr :string = temp["detectedstate"];
+    //       let tempDistrictstr :string = temp["detecteddistrict"];
+    //       if(  tempStatestr.toLowerCase() == this.searchStateTerm.toLowerCase()
+    //         && tempDistrictstr.toLowerCase() == this.searchDistrictTerm.toLowerCase()
+    //       ){
+  
+    //         this.filteredData.push(this.totalData[index]);
+  
+    //       }
+         
+    //     }
+  
+    //     console.log(this.filteredData);
+    //     this.getRecoveryStatus();
+    //     this.status = true;
+		// document.getElementById("Patientstable").focus();
+  
+    //   });
     }
     else{
       this.searchStateTerm = "";
-      console.log("Nothing to query");
+      //console.log("Nothing to query");
     }
     
   }
@@ -109,8 +176,17 @@ export class AppComponent {
 
 
   setDistrict(district:string){
-    console.log("district",district);
+    //console.log("district",district);
     this.searchDistrictTerm = district;
+  }
+
+  setZone(zone:string){
+    console.log("zone:",zone);
+    if(zone == ""){
+      this.zoneTerm = 'GREEN';
+    }else{
+      this.zoneTerm = zone.toUpperCase();
+    }
   }
 
 
@@ -128,21 +204,61 @@ export class AppComponent {
     this.deceased = 0;
     this.statusNotDisclosed =  0;
 
-    for (let index = 0; index < this.filteredData.length; index++) {
+    this.covid.getStateDistrictData().subscribe(response => {
       
-      console.log(this.filteredData[index].currentstatus);
+      let state = this.filteredData[0].detectedstate;
+      let district = this.filteredData[0].detecteddistrict;
 
-      if(this.filteredData[index].currentstatus == 'Recovered'){
-          this.recovered++;
-      }else if(this.filteredData[index].currentstatus == 'Hospitalized'){
-          this.hospitalized++;
-      }else if(this.filteredData[index].currentstatus == '') {
-          this.statusNotDisclosed++;
-      }else if(this.filteredData[index].currentstatus == 'Deceased'){
-          this.deceased++;
-      }
+      console.log("state district",state," ",district)
+
+      let responseLen = Object.keys(response).length
+
+      console.log(response);
+      for (let index = 0; index < responseLen; index++) {
+        
+        if(response[index].state == state){
+            console.log("in state if :",state);
+            
+          for (let index2 = 0; index2 < response[index].districtData.length; index2++) {
+            console.log("response[index].districtData[index2].district",response[index].districtData[index2].district);
+            
+            if(response[index].districtData[index2].district == district){
+
+              console.log("in district if :",district);
+              console.log(response[index].districtData[index2].confirmed);
+              console.log(response[index].districtData[index2].recovered);
+              console.log(response[index].districtData[index2].active);
+              console.log(response[index].districtData[index2].deceased);
+              this.confirmed = response[index].districtData[index2].confirmed;
+              this.recovered = response[index].districtData[index2].recovered;
+              this.hospitalized = response[index].districtData[index2].active;
+              this.deceased = response[index].districtData[index2].deceased;
+            }
+            
+          }
       
-    }
+        }
+        
+      }
+
+    });
+
+
+    // for (let index = 0; index < this.filteredData.length; index++) {
+      
+    //   console.log(this.filteredData[index].currentstatus);
+
+    //   if(this.filteredData[index].currentstatus == 'Recovered'){
+    //       this.recovered++;
+    //   }else if(this.filteredData[index].currentstatus == 'Hospitalized'){
+    //       this.hospitalized++;
+    //   }else if(this.filteredData[index].currentstatus == '') {
+    //       this.statusNotDisclosed++;
+    //   }else if(this.filteredData[index].currentstatus == 'Deceased'){
+    //       this.deceased++;
+    //   }
+      
+    // }
 
   }
 
--- src/app/covid-data.service.ts
+++ src/app/covid-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { pluck } from 'rxjs/operators';
+import {forkJoin} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,11 +23,19 @@ export class CovidDataService {
 
   getCovidData(){
 
-    return this.http.get("https://api.covid19india.org/raw_data.json");
+    let response1 = this.http.get("https://api.covid19india.org/raw_data1.json");
+    let response2 = this.http.get("https://api.covid19india.org/raw_data2.json");
+    let response3 = this.http.get("https://api.covid19india.org/raw_data3.json");
+    // Observable.forkJoin (RxJS 5) changes to just forkJoin() in RxJS 6
+    return forkJoin([response1, response2, response3]);
 
   }
-  
 
+  getStateDistrictData(){
+    return this.http.get("https://api.covid19india.org/v2/state_district_wise.json");
+  }
+  
+  
 
 
 }
